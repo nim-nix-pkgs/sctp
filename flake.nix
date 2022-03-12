98fca5098b6bb26075690b03bcbaf04bc9ccb221
hugosenari <hugosenari@gmail.com>
2022-03-12 03:59:11 -0300

    chore: re index sctp

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Userspace SCTP bindings'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."sctp-master".url = "path:./master";
+  inputs."sctp-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."sctp-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''Userspace SCTP bindings'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-sctp-master.flake = false;
+  inputs.src-sctp-master.owner = "metacontainer";
+  inputs.src-sctp-master.ref   = "refs/heads/master";
+  inputs.src-sctp-master.repo  = "sctp.nim";
+  inputs.src-sctp-master.type  = "github";
+  
+  inputs."reactor".dir   = "nimpkgs/r/reactor";
+  inputs."reactor".owner = "riinr";
+  inputs."reactor".ref   = "flake-pinning";
+  inputs."reactor".repo  = "flake-nimble";
+  inputs."reactor".type  = "github";
+  inputs."reactor".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."reactor".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-sctp-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-sctp-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
