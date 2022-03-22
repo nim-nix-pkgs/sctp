34a1084254f36885db31974b4fa60333d37b360e
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:15 -0300

    chore: re-index ../nimpkgs/s/sctp

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Userspace SCTP bindings'';
+  description = ''High-level Nim bindings for userspace SCTP implementation'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,13 +9,14 @@
   
   inputs.src-sctp-master.flake = false;
   inputs.src-sctp-master.owner = "metacontainer";
-  inputs.src-sctp-master.ref   = "refs/heads/master";
+  inputs.src-sctp-master.ref   = "master";
   inputs.src-sctp-master.repo  = "sctp.nim";
   inputs.src-sctp-master.type  = "github";
   
   inputs."reactor".owner = "nim-nix-pkgs";
   inputs."reactor".ref   = "master";
   inputs."reactor".repo  = "reactor";
+  inputs."reactor".dir   = "v0_5_0";
   inputs."reactor".type  = "github";
   inputs."reactor".inputs.nixpkgs.follows = "nixpkgs";
   inputs."reactor".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
